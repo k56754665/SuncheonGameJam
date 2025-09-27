09fc3ad3ca1efd417869efad4e5d4518969fb206
gjkang <95170766+k56754665@users.noreply.github.com>
2025-09-27 22:24:07 +0900

    [FEAT] 돈 UI 팝업 추가

--- SuncheonGameJam/Assets/Scripts/KGJ/UI_Money.cs
+++ SuncheonGameJam/Assets/Scripts/KGJ/UI_Money.cs
@@ -5,8 +5,10 @@ public class UI_Money : MonoBehaviour
 {
     [SerializeField] private Transform moneyRoot;
     [SerializeField] private TMP_Text moneyText;
+    
     private GameObject _giftMoneyPrefab;
     private MoneyManager _moneyManager;
+    private MoneyPopupText _moneyPopupText;
     
     private void Awake()
     {
@@ -15,15 +17,22 @@ public class UI_Money : MonoBehaviour
 
     private void Start()
     {
+        _moneyPopupText = GetComponentInChildren<MoneyPopupText>();
         _moneyManager = MoneyManager.Instance;
-        _moneyManager.OnMoneyAdded += UpdateMoneyText;
         _moneyManager.OnMoneyAdded += MakeMoney;
+        _moneyManager.OnMoneyAdded += ShowMoneyPopup;
         _moneyManager.OnMoneyRemoved += UpdateMoneyText;
+        UpdateMoneyText(_moneyManager.CurrentMoney);
     }
 
+    private void ShowMoneyPopup(float amount)
+    {
+        _moneyPopupText.ShowPopup(amount);
+    }
+    
     private void UpdateMoneyText(float amount)
     {
-        moneyText.text = $"순천사랑상품권 : {MoneyManager.Instance.CurrentMoney}";
+        moneyText.text = $"보유금 : {MoneyManager.Instance.CurrentMoney:N0}원";
     }
 
     private int GetGiftCount(float amount)
@@ -37,24 +46,34 @@ public class UI_Money : MonoBehaviour
     private void MakeMoney(float amount)
     {
         int count = GetGiftCount(amount);
+        bool executed = false; // 한 번만 실행되도록 플래그
 
         for (int i = 0; i < count; i++)
         {
             var go = Instantiate(_giftMoneyPrefab, transform);
             var rt = go.GetComponent<RectTransform>();
-            rt.anchoredPosition = Vector2.zero; // 중앙에서 시작
+            rt.anchoredPosition = Vector2.zero;
 
             var giftMoney = go.GetComponent<UI_GiftMoney>();
 
-            // 이펙트 실행
+            giftMoney.OnEffectComplete += () =>
+            {
+                if (executed) return; // 이미 실행했다면 무시
+                executed = true;
+
+                //ShowMoneyPopup(amount);
+                UpdateMoneyText(amount);
+            };
+
             giftMoney.PlayEffect(moneyRoot.position);
         }
     }
 
+    
     private void OnDestroy()
     {
-        _moneyManager.OnMoneyAdded -= UpdateMoneyText;
         _moneyManager.OnMoneyAdded -= MakeMoney;
+        _moneyManager.OnMoneyAdded -= ShowMoneyPopup;
         _moneyManager.OnMoneyRemoved -= UpdateMoneyText;
     }
 }
